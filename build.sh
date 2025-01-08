d8c571c160ddf8098245aceb39ac222f32dffcfc
Diyorbek Sadullaev <diyorbek3377@gmail.com>
2025-01-08 14:06:00 +0500

    static link works on macos

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
-mode=Debug # Release
-
+mode=Release # Release
 
 if [[ "$OS" == *Windows* ]]; then
     cmake -S . -B build -DCMAKE_BUILD_TYPE=$mode -G "MinGW Makefiles"
+    # Make ctgui static
+    sed -i 's/ctgui SHARED/ctgui/' build/_deps/ctgui-src/src/CTGUI/CMakeLists.txt
     cmake --build build --config $mode
 
     # find build/_deps/*-build -name '*.dll' | xargs cp -t build
 else
     cmake -S . -B build -DCMAKE_BUILD_TYPE=$mode
+    # Make ctgui static
+    sed -i '' 's/ctgui SHARED/ctgui/' build/_deps/ctgui-src/src/CTGUI/CMakeLists.txt
     cmake --build build --config $mode
 fi
