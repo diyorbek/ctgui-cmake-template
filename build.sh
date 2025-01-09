7f249f3f53326b6d227e2d2df92e67d41af40d35
Diyorbek Sadullaev <diyorbek3377@gmail.com>
2025-01-09 13:32:04 +0500

    link freetype statically on macos

--- build.sh
+++ build.sh
@@ -1,15 +1,11 @@
-mode=Release # Release
+mode=Debug # Release
 
 if [[ "$OS" == *Windows* ]]; then
     cmake -S . -B build -DCMAKE_BUILD_TYPE=$mode -G "MinGW Makefiles"
-    # Make ctgui static
-    sed -i 's/ctgui SHARED/ctgui/' build/_deps/ctgui-src/src/CTGUI/CMakeLists.txt
     cmake --build build --config $mode
 
     # find build/_deps/*-build -name '*.dll' | xargs cp -t build
 else
     cmake -S . -B build -DCMAKE_BUILD_TYPE=$mode
-    # Make ctgui static
-    sed -i '' 's/ctgui SHARED/ctgui/' build/_deps/ctgui-src/src/CTGUI/CMakeLists.txt
     cmake --build build --config $mode
 fi
