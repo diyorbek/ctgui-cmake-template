7c94c86284a6ab07cb827193e7a1fd0897f1f1be
Diyorbek Sadullaev <diyorbek3377@gmail.com>
2025-01-09 15:25:44 +0500

    support static csfml build on windows

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@ mode=Debug # Release
 
 if [[ "$OS" == *Windows* ]]; then
     cmake -S . -B build -DCMAKE_BUILD_TYPE=$mode -G "MinGW Makefiles"
+    
+    # Build CSFML static library by modifying below files
+    cp -f support/Config.h build/_deps/csfml-src/include/SFML/Config.h
+    cp -f support/Macros.cmake.in build/_deps/csfml-src/cmake/Macros.cmake 
+
     cmake --build build --config $mode
 
     # find build/_deps/*-build -name '*.dll' | xargs cp -t build
